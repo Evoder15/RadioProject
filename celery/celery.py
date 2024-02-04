ae71b4a1a729d24b4572c2a05a1167b0fee157b1
Debojit Pushilal <pushilald@gmail.com>
2024-02-04 22:55:20 +0530

    Finished setting up Dev Environment

--- celery/celery.py
+++
@@ -1,9 +0,0 @@
-# celery.py
-
-from celery import Celery
-from celeryconfig import BROKER_URL, CELERY_RESULT_BACKEND
-
-app = Celery('celery', broker=BROKER_URL, backend=CELERY_RESULT_BACKEND)
-
-# Include tasks from all modules in the tasks package
-app.autodiscover_tasks(['tasks'])
--- celery/celeryconfig.py
+++
@@ -1,4 +0,0 @@
-# celeryconfig.py
-
-BROKER_URL = 'pyamqp://guest:guest@rabbitmq:5672//'
-CELERY_RESULT_BACKEND = 'mongodb://mongodb:27017/celery_results'
